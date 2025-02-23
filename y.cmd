9c71b335f14b5c9bde447c8f92f83d516f0a8d3e
Konstantin Kulakov <16433297+kvk-2015@users.noreply.github.com>
2025-02-23 05:03:42 +0500

    Update y.cmd

--- y.cmd
+++ y.cmd
@@ -1,7 +1,7 @@
 @echo off
 chcp 65001 >nul
 setlocal
-set VideoURL=https://smotrim.ru/video/1952046
+set VideoURL=https://www.1tv.ru/doc/pro-zhizn-zamechatelnyh-lyudey/ya-ne-zhaleyu-ni-o-chem-dokumentalnyy-film-k-80-letiyu-yuriya-antonova
 set template=%%(title)s [%%(id)s].%%(ext)s
 set format=b
 set extension=mov
@@ -16,8 +16,14 @@ set /p filename=<%tempFileName%
 set filename=%filename:.mp4=.mov%.txt
 echo %VideoURL% > "%filename%" && del /q %tempFileName%
 echo.>> "%filename%"
+call :size "%filename%"
+set tempsize=%filsize%
 call %AppPath% --socket-timeout 45 --print formats_table %%VideoURL%% >> "%filename%"
 if not errorlevel 0 exit /b
+call :size "%filename%"
+if %tempsize% == %filsize% exit /b
 if -%1- == ---- exit /b
 rem --limit-rate 8.5M
 start "yt-dlp: %VideoURL%" %AppPath% -o "%template%" --split-chapters --video-multistreams --audio-multistreams --windows-filenames --remux-video %extension% --concurrent-fragments 10 --socket-timeout 45 --abort-on-unavailable-fragment --exec "pause " --embed-metadata --format %format% %VideoURL% ^&exit/b
+:size
+set filsize=%~z1
