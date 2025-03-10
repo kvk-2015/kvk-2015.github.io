3245db4d053c8167e97485a3c1785e0a113eb8aa
Konstantin Kulakov <16433297+kvk-2015@users.noreply.github.com>
2025-03-10 13:17:31 +0500

    Update y.cmd

--- y.cmd
+++ y.cmd
@@ -1,8 +1,8 @@
 @echo off
 chcp 65001 >nul
 setlocal
-set VideoURL=https://www.1tv.ru/doc/pro-zhizn-zamechatelnyh-lyudey/ya-ne-zhaleyu-ni-o-chem-dokumentalnyy-film-k-80-letiyu-yuriya-antonova
-set template=%%(title)s [%%(id)s].%%(ext)s
+set VideoURL=https://smotrim.ru/video/2943847
+set template=M1.%%(title)s [%%(id)s].%%(ext)s
 set format=b
 set extension=mov
 set AppPath=D:\kvk\Utilities\GitHub\yt-dlp\yt-dlp.cmd
@@ -17,13 +17,13 @@ set filename=%filename:.mp4=.mov%.txt
 echo %VideoURL% > "%filename%" && del /q %tempFileName%
 echo.>> "%filename%"
 call :size "%filename%"
-set tempsize=%filsize%
+set tempsize=%filesize%
 call %AppPath% --socket-timeout 45 --print formats_table %%VideoURL%% >> "%filename%"
 if not errorlevel 0 exit /b
 call :size "%filename%"
-if %tempsize% == %filsize% exit /b
+if %tempsize% == %filesize% exit /b
 if -%1- == ---- exit /b
 rem --limit-rate 8.5M
 start "yt-dlp: %VideoURL%" %AppPath% -o "%template%" --split-chapters --video-multistreams --audio-multistreams --windows-filenames --remux-video %extension% --concurrent-fragments 10 --socket-timeout 45 --abort-on-unavailable-fragment --exec "pause " --embed-metadata --format %format% %VideoURL% ^&exit/b
 :size
-set filsize=%~z1
+set filesize=%~z1
