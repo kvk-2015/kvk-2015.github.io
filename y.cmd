0c467becf88eb86bacf13f911c952fba3d4028e1
Konstantin Kulakov <16433297+kvk-2015@users.noreply.github.com>
2025-09-15 02:25:45 +0500

    Update y.cmd

--- y.cmd
+++ y.cmd
@@ -1,7 +1,9 @@
 @echo off
 chcp 65001 >nul
-setlocal enabledelayedexpansion
+setlocal
 set VideoURL=https://smotrim.ru/video/3019278
+set head=
+set suffix=
 set series=%%(series)s. 
 call :set_template
 set format=b
@@ -10,16 +12,16 @@ set AppPath=D:\kvk\Utilities\GitHub\yt-dlp\yt-dlp.cmd
 if not exist %AppPath% set AppPath=yt-dlp.exe
 if not -%1- == -- set format=%1
 set tempFileName=%random%.tmp
-set description_file_name=%template:^!=%
-set description_file_name=%description_file_name:..=.%
-call %AppPath% -o "%%description_file_name%%" --windows-filenames --socket-timeout 45 --print-to-file filename %%tempFileName%% --skip-download %%VideoURL%%
+call %AppPath% -o "%%template:.!=%%" --windows-filenames --socket-timeout 45 --print-to-file filename %%tempFileName%% --skip-download %%VideoURL%%
 if not errorlevel 0 if exist %tempFileName% del /q %tempFileName%
 if not exist %tempFileName% exit /b
 set /p filename=<%tempFileName%
 set filename_without_series=%filename:NA. =%
 if not "%filename_without_series%" == "%filename%" (set series=& call :set_template & set filename=%filename_without_series%)
+setlocal enabledelayedexpansion
 set filename=!filename:.mp4=.%extension%!
 set filename=!filename:.webm=.%extension%!.txt
+setlocal disabledelayedexpansion
 echo %VideoURL% > "%filename%" && del /q %tempFileName%
 echo.>> "%filename%"
 call :size "%filename%"
@@ -32,7 +34,7 @@ if -%1- == ---- exit /b
 rem --limit-rate 8.5M
 start "yt-dlp: %VideoURL%" %AppPath% -o "%template%" --split-chapters --postprocessor-args "SplitChapters+ffmpeg:-map_metadata -1" --video-multistreams --audio-multistreams --windows-filenames --remux-video %extension% --concurrent-fragments 10 --socket-timeout 45 --abort-on-unavailable-fragment --exec "pause " --embed-metadata --format %format% %VideoURL% ^&exit/b
 :set_template
-set template=%series%%%(title)s [%%(id)s].%%(ext)s
+set template=%head%%series%%%(title)s [%%(id)s]%suffix%.%%(ext)s
 exit /b
 :size
 set filesize=%~z1
